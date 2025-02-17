a4536e3402048a2068632ca0df42fd4c7a7f2f9a
Enzo Davico <enzo.davico.p@gmail.com>
2025-02-17 10:58:18 -0300

    fix: remove broken separators

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -147,7 +147,7 @@ require("lualine").setup {
   options = {
     icons_enabled = true,
     theme = "auto",
-    component_separators = { left = "⏐", right = "⏐" },
+    component_separators = "",
     section_separators = "",
     disabled_filetypes = {},
     always_divide_middle = true,
